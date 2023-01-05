b35d72a084f636bfc0a0669eec01225071aebf2c
Radoslav Dimitrov <rdimitrov@indeavr.com>
2023-01-05 16:41:04 +0200

    Added image, image_type tables; added box columns to resource, resource_group; added ImageTypeManager, ImageManager and tests

--- utils/login.sh
+++ utils/login.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# $1 - email [default: test@example.com]
+# $2 - password [default: 1234Aa!11111]
+
+email=${1:-test@example.com}
+password=${2:-1234Aa!11111}
+
+#!/bin/bash
 curl -v \
 	http://localhost:5000/login/ \
 	-X POST \
 	-H "Content-Type: application/json" \
-	-d '{"email": "test@example.com", "password": "1234Aa!11111"}'
+	-d "{\"email\": \"$email\", \"password\": \"$password\"}"
 
