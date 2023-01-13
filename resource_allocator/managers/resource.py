d549b23b0fe642e679c425ff0ef28c6d14fce242
Radoslav Dimitrov <rdimitrov@indeavr.com>
2023-01-13 18:02:15 +0200

    Work on having nested managers

--- resource_allocator/managers/resource.py
+++ resource_allocator/managers/resource.py
@@ -9,14 +9,21 @@ from resource_allocator.models import (
 )
 
 from resource_allocator.managers.base import BaseManager
+from resource_allocator.managers.image import ImageManager
 
 
 class ResourceManager(BaseManager):
     model = ResourceModel
+    nested_managers = {
+        "image": ImageManager,
+    }
 
 
 class ResourceGroupManager(BaseManager):
     model = ResourceGroupModel
+    nested_managers = {
+        "image": ImageManager,
+    }
 
 
 class ResourceToResourceGroupManager(BaseManager):
