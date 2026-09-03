06b002ab1d8dc414be0e0c0335be4c515254b87d
Bugzey <radddi@abv.bg>
2026-09-03 15:45:03 +0300

    Added port option to startup.sh that defaults to 8000

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python -m gunicorn \
 	"resource_allocator.main:create_app()" \
-	-b 0.0.0.0:5000 \
+	-b 0.0.0.0:${PORT:-8000} \
 	-w "${NUM_WORKERS:-4}" \
 	--access-logfile "-" \
 	--error-logfile "-"
